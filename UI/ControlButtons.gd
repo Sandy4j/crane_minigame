c1b412b3475c417eb756b30e1e71d0279eef751d
Sanzkuy <yogasandy196@gmail.com>
2026-04-16 20:36:38 +0700

    feat: add texture to ui button

--- UI/ControlButtons.gd
+++ UI/ControlButtons.gd
@@ -1,22 +1,22 @@
-extends HBoxContainer
+extends Control
 
-@onready var grab_button = $GrabButton
-@onready var left_button = $LeftButton
-@onready var right_button = $RightButton
-
-func _on_grab_button_pressed():
-	_simulate_action("grab")
-
-
-func _on_left_button_pressed():
-	_simulate_action("left")
-
-
-func _on_right_button_pressed():
-	_simulate_action("right")
-
+@onready var grab_button = $GrabBtn
+@onready var left_button = $LeftBtn
+@onready var right_button = $RightBtn
 
 func _simulate_action(action_name: String):
 	Input.action_press(action_name)
 	await get_tree().process_frame
 	Input.action_release(action_name)
+
+func _on_left_btn_pressed():
+	_simulate_action("left")
+
+
+func _on_right_btn_pressed():
+	_simulate_action("right")
+
+
+func _on_grab_btn_pressed():
+	_simulate_action("grab")
+
